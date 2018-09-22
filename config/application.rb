40f250513eb85a0df9ca77599a7d21c108fc882f
m <mohka@protonmail.com>
2018-09-22 10:24:21 -0700

    added security headers to application.rb

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,18 @@ module Ifme
       end
     }
 
+    
+    config.action_dispatch.default_headers = {
+  'X-Frame-Options' => 'SAMEORIGIN',
+  'X-XSS-Protection' => '1; mode=block',
+  'X-Content-Type-Options' => 'nosniff',
+  'X-Download-Options' => 'noopen',
+  'X-Permitted-Cross-Domain-Policies' => 'none',
+  'Referrer-Policy' => 'strict-origin-when-cross-origin',
+  'Strict-Transport-Security' => 'max-age=31536000'
+    }
+    
+
     # gzip the html/json responses
     config.middleware.use Rack::Deflater, include: %w[text/html application/json image/svg+xml]
     # export translations for use in javascript
